40c0081597403714943a446b5f9eec83578f4c68
Gosho69 <127147526+Gosho69@users.noreply.github.com>
2023-03-09 10:21:07 +0200

    set up-v2

--- react-app-dream-team/src/firebase.js
+++ react-app-dream-team/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
+    apiKey: "AIzaSyDsEm5eMPpLNen_sgt-xv6uctjarfgVwDQ",
+    authDomain: "qwerty-c00b4.firebaseapp.com",
+    projectId: "qwerty-c00b4",
+    storageBucket: "qwerty-c00b4.appspot.com",
+    messagingSenderId: "381327451148",
+    appId: "1:381327451148:web:d1ca0696d65e8082ebfa29",
+    measurementId: "G-ZGZK75TCPN"
 };
 
 const app = initializeApp(firebaseConfig);
