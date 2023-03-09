e3f962df611fafa695cda4b784f890b9f2f6d2ec
Gosho69 <127147526+Gosho69@users.noreply.github.com>
2023-03-09 15:09:57 +0200

    login

--- react-app-dream-team/src/App.jsx
+++ react-app-dream-team/src/App.jsx
@@ -1,6 +1,7 @@
 import './firebase.js';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import './pages/Login';
 import Login from './pages/Login';
 const App = () => {
