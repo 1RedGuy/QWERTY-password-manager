36831025904af76037e66968dac848bdc7af1500
1RedGuy <119105124+1RedGuy@users.noreply.github.com>
2023-03-09 13:44:42 +0200

    bootsrap-change

---
+++ react-app-dream-team/src/App.jsx
@@ -0,0 +1,11 @@
+import './firebase.js';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './pages/Login';
+import Login from './pages/Login';
+const App = () => {
+   return(
+     <Login/>
+   );
+}
+export default App;
---
+++ react-app-dream-team/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css';
+const Login = () => {
+  return (
+    <div className="container text-center border border-1">
+       <form className='py-3'>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <button type="submit" className="btn btn-primary ">Submit</button>
+</form>
+    </div>
+   
+  );
+}
+export default Login;
