406ff146f8a6505f4eed2611edd98023ff8adade
Gosho69 <127147526+Gosho69@users.noreply.github.com>
2023-03-09 23:32:25 +0200

    about_us-page-ready-mostly

--- react-app-dream-team/src/pages/About_us.jsx
+++ react-app-dream-team/src/pages/About_us.jsx
@@ -8,10 +8,24 @@ const About_us = () =>{
     <div className="container">
         <h1 className="fs-1 text-center mt-20">Who are we and what is our idea?</h1>
         <Row md={4}>
-        <Col><img src="../img/profile-icon.png"/></Col>
-        <Col xs={6}>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col className="position-absolute top-50 start-0 translate-middle-y">
+            <img src={image} className="img-fluid"/>
+              <h2>Vladimir Demirev</h2>     
+            </Col>
+        <Col className="position-absolute top-50 start-50 translate-middle" xs={6}>
+            <img src={image} className="img-fluid" />
+            <h2>Georgi Baladzhanov</h2>
+            </Col>
+        <Col className="position-absolute top-50 end-0 translate-middle-y">
+            <img src={image} className="img-fluid" />
+            <h2>Petar Kerezov</h2>
+            </Col>
       </Row>
+      <p
+       className= "lh-sm text-center fs-4 text position-absolute bottom-0 start-50 translate-middle-x"
+       >We are students from Tues at age of 14/15 , creating a website for secure saving of passwords.Each of us have created different parts of the design and the code for the website to work properly.
+        Our technologies are Bootstrap and React for our website.
+      </p>
     </div>
 
     );
