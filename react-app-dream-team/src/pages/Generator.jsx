fffeff61e17ace6ceeec4d634f0e893c79834d3c
1RedGuy <119105124+1RedGuy@users.noreply.github.com>
2023-03-10 15:24:24 +0200

    generator design

--- react-app-dream-team/src/pages/Generator.jsx
+++ react-app-dream-team/src/pages/Generator.jsx
@@ -1,38 +1,32 @@
-import img from "../img/home-background.jpg";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../components/style-home/home.css";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/esm/Container";
 import "../components/style-home/generator.css";
-import  Header from "../components/header/Header";
+import Header from "../components/header/Header";
 const Generator = () => {
   return (
-    <div>
-       <Header/>
+    <div className = "thingy">
+      <Header />
       <div className="form-container">
         <Form className="form-div">
           <h1>Password Generator</h1>
           <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Label >Generated password: </Form.Label>
+            <Form.Control type="email" readOnly/>
           </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group className = "checkboxes" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Allow Numbers (0-9)" />
+            <Form.Check type="checkbox" label="Allow Uppercase (ABC)" />
+            <Form.Check type="checkbox" label="Allow Symbols (!@#$%^&*()+)" />
+            <Form.Check type="checkbox" label="Allow Lowercase (abc)" />
           </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
+          <div>
+            <Button className="generate-button" variant="primary" type="submit">
+            Generate Password
           </Button>
+          </div>
         </Form>
       </div>
     </div>
