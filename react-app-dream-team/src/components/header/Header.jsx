a3b70506282c3a945eae60d8221b7997732cd63d
PepiKerezov <petar.a.kerezov.2022@elsys-bg.org>
2023-03-10 16:19:41 +0200

    small

--- react-app-dream-team/src/components/header/Header.jsx
+++ react-app-dream-team/src/components/header/Header.jsx
@@ -8,28 +8,28 @@ import "./Header.css";
 const Header = () => {
   return (
     <div>
-      <Navbar  className="header-navbar">
-          <Container className="nav-container mr = 20px">
-            <Navbar.Brand className="my-logo" href="/home">
-              QWERTY
-            </Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link className="generator" href="/generator">
-                Generator
-              </Nav.Link>
-              <Nav.Link className="passwords" href="#passwords">
-                Passwords
-              </Nav.Link>
-              <Nav.Link className="about-us" href="/about-us">
-                About us
-              </Nav.Link>
-              <Nav.Link className="sign-out" href="/sign-out">
-                Sign Out
-              </Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </div>
+      <Navbar className="header-navbar">
+        <Container className="nav-container mr = 20px">
+          <Navbar.Brand className="my-logo" href="/home">
+            QWERTY
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link className="generator" href="/generator">
+              Generator
+            </Nav.Link>
+            <Nav.Link className="passwords" href="/passwords">
+              Passwords
+            </Nav.Link>
+            <Nav.Link className="about-us" href="/about-us">
+              About us
+            </Nav.Link>
+            <Nav.Link className="sign-out" href="/sign-out">
+              Sign Out
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 export default Header;
