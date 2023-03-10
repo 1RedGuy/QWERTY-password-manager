a5a9982760fcc4f497df86889eca86a3c7fa9011
Gosho69 <127147526+Gosho69@users.noreply.github.com>
2023-03-10 11:36:02 +0200

    about-us-ready

--- react-app-dream-team/src/firebase.js
+++ react-app-dream-team/src/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
-
+import env from "react-dotenv";
 const firebaseConfig = {
-    apiKey: "AIzaSyDsEm5eMPpLNen_sgt-xv6uctjarfgVwDQ",
-    authDomain: "qwerty-c00b4.firebaseapp.com",
-    projectId: "qwerty-c00b4",
-    storageBucket: "qwerty-c00b4.appspot.com",
-    messagingSenderId: "381327451148",
-    appId: "1:381327451148:web:d1ca0696d65e8082ebfa29",
-    measurementId: "G-ZGZK75TCPN"
+apiKey:env.REACT_APP_apiKey,
+AuthDomain:env.React_authDomain, 
+projectId:env.React_projectId,
+storageBucket:env.React_storageBucket,
+messagingSenderId:env.React_messagingSenderId,
+appId:env.React_appId,
+measurementId:env.React_measurementId
+
 };
 
 const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app)
+export const firestore = getFirestore(app);
--- react-app-dream-team/src/pages/About_us.jsx
+++ react-app-dream-team/src/pages/About_us.jsx
@@ -1,33 +1,94 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import image from "../img/profile-icon.png";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-const About_us = () =>{
-    return(
-    <div className="container">
-        <h1 className="fs-1 text-center mt-20">Who are we and what is our idea?</h1>
-        <Row md={4}>
-        <Col className="position-absolute top-50 start-0 translate-middle-y">
-            <img src={image} className="img-fluid"/>
-              <h2>Vladimir Demirev</h2>     
-            </Col>
-        <Col className="position-absolute top-50 start-50 translate-middle" xs={6}>
-            <img src={image} className="img-fluid" />
-            <h2>Georgi Baladzhanov</h2>
-            </Col>
-        <Col className="position-absolute top-50 end-0 translate-middle-y">
-            <img src={image} className="img-fluid" />
-            <h2>Petar Kerezov</h2>
-            </Col>
-      </Row>
-      <p
-       className= "lh-sm text-center fs-4 text position-absolute bottom-0 start-50 translate-middle-x"
-       >We are students from Tues at age of 14/15 , creating a website for secure saving of passwords.Each of us have created different parts of the design and the code for the website to work properly.
-        Our technologies are Bootstrap and React for our website.
-      </p>
-    </div>
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import image1 from "../img/home-background.jpg";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/esm/Container";
+import "./About_us.css";
 
-    );
-}
+const About_us = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${image1})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        objectFit: "cover",
+        width: "100vw",
+        height: "100vh",
+        margin: "0 0",
+      }}
+      className=""
+    >
+      <div className="nav-bar">
+        <Navbar className="bg-transparent" bg="dark" variant="dark">
+          <Container className="nav-container mr = 20px">
+            <Navbar.Brand className="my-logo" href="#home">
+              QWERTY
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link className="generator" href="#about-us">
+                Generator
+              </Nav.Link>
+              <Nav.Link className="passwords" href="#passwords">
+                Passwords
+              </Nav.Link>
+              <Nav.Link className="about-us" href="#about-us">
+                About us
+              </Nav.Link>
+              <Nav.Link className="sign-out" href="#sign-out">
+                Sign Out
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+
+      <div  className="about-us-container text-light text-center">
+        <div className="component-container">
+          <h1 className=" py-10 fs-1 ">
+            Who are we and what is our idea?
+          </h1>
+        </div>
+
+        <div className="component-container">
+          <Row className="about-us-profiles">
+            <Col className="">
+              <img src={image} className="profile-img" />
+              <h2>Vladimir Demirev</h2>
+            </Col>
+
+            <Col className="  ">
+              <img
+                src={image}
+                className="profile-img"
+              />
+              <h2>Georgi Baladzhanov</h2>
+            </Col>
+
+            <Col className="">
+              <img
+                src={image}
+                className="profile-img"
+              />
+              <h2>Petar Kerezov</h2>
+            </Col>
+          </Row>
+        </div>
+
+        <div className=" info component-container text-center"> 
+          <p className="">
+            We are students from Tues at age of 14/15 , creating a website for
+            secure saving of passwords.Each of us have created different parts
+            of the design and the code for the website to work properly. Our
+            technologies are Bootstrap and React for our website.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default About_us;
