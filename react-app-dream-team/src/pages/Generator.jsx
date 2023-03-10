66461205bf070b500532ef5aa706d40fe37c44db
1RedGuy <119105124+1RedGuy@users.noreply.github.com>
2023-03-10 16:41:10 +0200

    generator

--- react-app-dream-team/src/pages/Generator.jsx
+++ react-app-dream-team/src/pages/Generator.jsx
@@ -5,25 +5,87 @@ import Form from "react-bootstrap/Form";
 import "../components/style-home/home.css";
 import "../components/style-home/generator.css";
 import Header from "../components/header/Header";
+import { useState } from "react";
 const Generator = () => {
+  const[generatedPassword, setGeneratedPassword] = useState("");
+  const[allowNumbers, setAllowNumbers] = useState(false);
+  const[allowUppercase, setAllowUppercase] = useState(false);
+  const[allowSymbols, setAllowSymbols] = useState(false);
+  const[allowLowercase, setAllowLowercase] = useState(false);
+
+  const handleCheckboxChange = (e) => {
+    const checkboxName = e.target.name;
+    const isChecked = e.target.checked;
+    switch (checkboxName) {
+      case "num_checkbox":
+        setAllowNumbers(isChecked);
+        break;
+      case "upc_checkbox":
+        setAllowUppercase(isChecked);
+        break;
+      case "symb_checkbox":
+        setAllowSymbols(isChecked);
+        break;
+      case "lwc_checkbox":
+        setAllowLowercase(isChecked);
+        break;
+      default:
+        break;
+    }
+  };   
+  const generatePassword = () => {
+  let char = "";
+  if (allowNumbers)char += "0123456789";
+  if (allowUppercase)char += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (allowSymbols)char += "!@#$%^&*()+";
+  if (allowLowercase)char += "abcdefghijklmnopqrstuvwxyz";
+  let password = "";
+  for (let i = 0; i < 10; i++) {
+    password += char.charAt(Math.floor(Math.random() * char.length));
+    }
+    setGeneratedPassword(password);
+  };
   return (
     <div className = "thingy">
       <Header />
       <div className="form-container">
         <Form className="form-div">
           <h1>Password Generator</h1>
-          <Form.Group controlId="formBasicEmail">
+          <Form.Group controlId="upper-part-form">
             <Form.Label >Generated password: </Form.Label>
-            <Form.Control type="email" readOnly/>
+            <Form.Control type="email" readOnly value = {generatedPassword}/>
           </Form.Group>
-          <Form.Group className = "checkboxes" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Allow Numbers (0-9)" />
-            <Form.Check type="checkbox" label="Allow Uppercase (ABC)" />
-            <Form.Check type="checkbox" label="Allow Symbols (!@#$%^&*()+)" />
-            <Form.Check type="checkbox" label="Allow Lowercase (abc)" />
+          <Form.Group className = "checkboxes">
+            <Form.Check 
+            type="checkbox"
+            label="Allow Numbers (0-9)"
+            name="num_checkbox"
+            onChange={handleCheckboxChange}
+             />
+            <Form.Check 
+            type="checkbox"
+            label="Allow Uppercase (ABC)"
+            name="upc_checkbox"
+            onChange={handleCheckboxChange}
+             />
+            <Form.Check
+            type="checkbox"
+            label="Allow Symbols (!@#$%^&*()+)" 
+            name="symb_checkbox"
+            onChange={handleCheckboxChange}/>
+            <Form.Check 
+            type="checkbox" 
+            label="Allow Lowercase (abc)"
+            name="lwc_checkbox"
+            onChange={handleCheckboxChange}
+            />
           </Form.Group>
           <div>
-            <Button className="generate-button" variant="primary" type="submit">
+            <Button className="generate-button"
+            variant="primary"
+            type="button"
+            onClick={generatePassword}
+            >
             Generate Password
           </Button>
           </div>
